9be9fd61a9c784dac9fa8756eb8268c07d64c468
Rahul Kumar <rahul@thoughtnirvana.com>
2018-12-23 00:03:35 +0530

    First commit

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.title = "Asteroids"
+    t.version = "11.2"
+end
---
+++ main.lua
@@ -0,0 +1,4 @@
+function love.draw()
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.circle('fill', 800 / 2, 600 / 2, 30)
+end
